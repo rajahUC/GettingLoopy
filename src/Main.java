5b548b5f568707d025f79b992aa000e4a160678f
rajahUC <rajaho@mail.uc.edu>
2023-06-26 12:03:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        /*
+        for (int i = 0; i < 31; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+
+
+
+        for (int i = 30; i >= 0; i--) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+
+
+
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.printf("%3d", i);
+        }
+
+
+
+        for (int i = 10; i >= 0 ; i -= 2) {
+            System.out.printf("%3d", i);
+        }
+
+
+
+        for (int i = 1; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+         */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
